2b74736eaa82dcad60d6a19ea729c3d89abdaec6
xevrion <krbavadiya11@gmail.com>
2025-07-26 12:09:18 +0530

    Init client and server setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Xendoro backend running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
